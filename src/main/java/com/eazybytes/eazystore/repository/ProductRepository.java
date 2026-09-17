9a842d96d0e76d59a59c3acf1b4bb1483bb97170
Varshini-501 <b220501@rgukt.ac.in>
2026-09-17 15:25:20 +0530

    Add method to find products by vendor ID
    Added method to find products by vendor ID and order by creation date.

--- src/main/java/com/eazybytes/eazystore/repository/ProductRepository.java
+++ src/main/java/com/eazybytes/eazystore/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.eazybytes.eazystore.repository;
 import com.eazybytes.eazystore.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
-  }
+
+    List<Product> findByVendor_VendorIdOrderByCreatedAtDesc(Long vendorId);
+}
